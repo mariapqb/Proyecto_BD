2d8b3bb94029cd8b882bdd237409414fc5d2151a
mariapqb <mariap.quintinb@uqvirtual.edu.co>
2026-05-25 21:10:58 -0500

    SQL Final 1.0

--- QuindioFlix_PRIV_ROL_ANALIS - copia.sql
+++
@@ -1,8 +0,0 @@
-GRANT SELECT ON quindioflix.usuarios TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.perfiles TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.contenido TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.reproducciones TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.calificaciones TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.pagos TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.reportes TO ROL_ANALISTA;
-GRANT SELECT ON quindioflix.planes TO ROL_ANALISTA;
